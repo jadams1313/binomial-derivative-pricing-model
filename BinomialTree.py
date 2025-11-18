e349b55ebf51d673faaba1ca5d27433900697512
Jake Adams <jba5676@psu.edu>
2025-11-18 10:45:35 -0500

    finished binomial asset pricing model and presentation

--- BinomialTree.py
+++ BinomialTree.py
@@ -1,7 +1,7 @@
 import numpy as np 
 
 class BinomialTree:
-    def __init__(self, S_0, T, K, r, u , d, N, usesCoxRosenstein, sigma=0):
+    def __init__(self, S_0, T, K, r, N, sigma=0):
         self.S_0 = S_0  # Initial stock price
         self.T = T      # Time to maturity
         self.K = K      # Strike price
@@ -14,43 +14,35 @@ class BinomialTree:
         self.p = (np.exp(r * self.dt) - self.d) / (self.u - self.d)  # Risk-neutral probability
 
     def stock_price(self, i, j):
-        """Calculate the stock price at node (i, j)"""
-        return round(self.S_0 * (self.u ** j) * (self.d ** (i - j)), 4)
+        return self.S_0 * (self.u ** j) * (self.d ** (i - j))
      
     def payoff(self, S, derivative_type):
-        """Calculate the payoff of the derivative at maturity"""
         if derivative_type == 'call':
             return max(0, S - self.K)
         else:
             return max(0, self.K - S)
         
     def option_price(self, derivative_type):
-        """Calculate the option price using the binomial tree model"""
         payoffs = np.zeros(self.N + 1)
-        values = np.zeros(self.N + 1)
-
-        # initialize asset prices at maturity
-        payoffs = np.zeros(self.N + 1)
-    
-        # Initialize asset prices at maturity
+        #initialize payoffs
         for i in range(self.N + 1):
             S = self.stock_price(self.N, i)
             payoffs[i] = self.payoff(S, derivative_type=derivative_type)
         
-        # Backward induction through the tree
+        #backward induction through the tree
         for i in range(self.N - 1, -1, -1):
             for j in range(i + 1):
-                payoffs[j] = round(np.exp(-self.r * self.dt) * (
+                payoffs[j] = np.exp(-self.r * self.dt) * (
                     self.p * payoffs[j + 1] + (1 - self.p) * payoffs[j]
-                ), 4) 
+                )
         
-        return payoffs[0]
+        return round(payoffs[0], 4)
 
 def black_scholes_call_price(S, K, T, r, sigma):
     from scipy.stats import norm
     from math import log, sqrt, exp
 
-    d1 = (log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
+    d1 = (log(S / K) + ((r + 0.5 * sigma ** 2) * T)) / (sigma * sqrt(T))
     d2 = d1 - sigma * sqrt(T)
 
     call_price = S * norm.cdf(d1) - K * exp(-r * T) * norm.cdf(d2)
